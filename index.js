13ec3661606e1abacc9b08b1e916beecaa301237
dnzcsgn <dnzcsgndc@gmail.com>
2025-05-19 19:53:33 -0700

    updated

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+require("datejs");
 
+function combineUsers(...userArrays) {
+  // Initialize combined object
+  const combinedObject = {
+    users: [],
+    merge_date: null,
+  };
 
+  // Merge all users
+  for (const array of userArrays) {
+    combinedObject.users.push(...array);
+  }
+
+  // Set current date in M/d/yyyy format
+  const currentDate = new Date().toString("M/d/yyyy");
+  combinedObject.merge_date = currentDate;
+
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
